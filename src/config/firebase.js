4052584f44a641adf84ff8d4219fce7c36fc93d2
Emmanuel Oluwatayese <adetutuemmanueloluwatayese@gmail.com>
2025-06-28 17:46:30 +0100

    feat: Initializing the instagram clone app

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,17 +3,51 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+const apiKey = import.meta.env.VITE_FIREBASE_API_KEY;
+const authDomain = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
+const projectId = import.meta.env.VITE_FIREBASE_PROJECT_ID;
+const storageBucket = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
+const appId = import.meta.env.VITE_FIREBASE_APP_ID;
+const measurementId = import.meta.env.VITE_FIREBASE_MEASUREMENT_ID;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCvydMWRc8ACtYWHmhrMu2emvr9EmmZZhA",
-  authDomain: "adming-panel-c0d51.firebaseapp.com",
-  projectId: "adming-panel-c0d51",
-  storageBucket: "adming-panel-c0d51.firebasestorage.app",
-  messagingSenderId: "1087891109119",
-  appId: "1:1087891109119:web:6d235e37283fea88e788ca",
-  measurementId: "G-YN0EE2BK7N"
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId
 };
 
 
+// ✅ Initialize Firebase app
 const app = initializeApp(firebaseConfig);
+
+// ✅ Initialize Firebase Auth and Provider
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+
+
+export const loginWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+    window.location.href = "/dashboard";
+    console.log("✅ Logged in user:", user);
+} catch (error) {
+    console.error("❌ Google sign-in failed:", error);
+}
+};
+
+export const logout = async () => {
+  try {
+      await signOut(auth);
+      console.log("✅ Logged out successfully");
+      window.location.href = "/";
+  } catch (error) {
+    console.error("❌ Logout error", error);
+  }
+};
