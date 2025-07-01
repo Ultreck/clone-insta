ba5c92fdb7eeea613a235094f295f3cc872102d3
Emmanuel Oluwatayese <adetutuemmanueloluwatayese@gmail.com>
2025-07-01 20:30:23 +0100

    feat: worked on responsiveness

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -22,7 +22,7 @@ function Sidebar({
     cLocation.pathname === path ? "text-pink-600" : "text-gray-800";
 
   return (
-    <aside className="fixed left-0 top-0 h-screen w-16 md:w-64 bg-white shadow flex flex-col justify-between p-4">
+    <aside className="fixed left-0 top-0 h-screen w-14 md:w-64 bg-white shadow flex flex-col justify-between p-4">
       <div>
         <h1 className="text-pink-600 text-2xl font-bold mb-6 hidden md:block">
           InstaClone
@@ -34,11 +34,11 @@ function Sidebar({
               "/"
             )} hover:text-pink-600`}
           >
-            <FiHome className="text-2xl" />{" "}
+            <FiHome className="lg:text-2xl text-xl" />{" "}
             <span className="hidden md:inline">Home</span>
           </Link>
           {/* <Link to="/create" className={`flex items-center space-x-2 ${active('/create')} hover:text-pink-600`}>
-            <FiPlusSquare className="text-2xl" /> <span className="hidden md:inline">Create</span>
+            <FiPlusSquare className="lg:text-2xl text-xl" /> <span className="hidden md:inline">Create</span>
           </Link> */}
           <CreatePostModal
             triger={
@@ -48,7 +48,7 @@ function Sidebar({
               "/create"
             )} hover:text-pink-600`}
               >
-                <FiPlusSquare className="text-2xl" />{" "}
+                <FiPlusSquare className="lg:text-2xl text-xl" />{" "}
                 <span className="hidden md:inline">Create</span>
               </div>
             }
@@ -72,7 +72,7 @@ function Sidebar({
               "/profile"
             )} hover:text-pink-600`}
           >
-            <FiUser className="text-2xl" />{" "}
+            <FiUser className="lg:text-2xl text-xl" />{" "}
             <span className="hidden md:inline">Profile</span>
           </Link>
           <Link
@@ -81,7 +81,7 @@ function Sidebar({
               "/notifications"
             )} hover:text-pink-600`}
           >
-            <FiBell className="text-2xl" />{" "}
+            <FiBell className="lg:text-2xl text-xl" />{" "}
             <span className="hidden md:inline">Notifications</span>
           </Link>
         </nav>
@@ -92,7 +92,7 @@ function Sidebar({
             onClick={onLogout}
             className="flex items-center space-x-2 text-red-600 hover:text-red-800"
           >
-            <FiLogOut className="text-2xl" />{" "}
+            <FiLogOut className="lg:text-2xl text-xl" />{" "}
             <span className="hidden md:inline">Logout</span>
           </button>
         )}
